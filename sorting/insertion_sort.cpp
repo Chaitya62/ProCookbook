b6abc63220985a9aa2249190e7821abd177167b1
Chaitya Shah <Chaitya62@gmail.com>
2016-05-21 16:18:10 +0530

    Add Insertion sort

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,45 @@
+/*Insertion Sort
+Written by Chaitya Shah
+*/
+#include<iostream>
+#include<stdlib.h> /* for malloc and calloc */
+#include<ctime>  /*for execution time */
+using namespace std;
+int start_watch;
+typedef long long datatype;  //replace long long with desired datatype
+int selection_sort(datatype *A,datatype N){
+    start_watch = clock();
+    datatype min,temp,pointer;
+    for(datatype i=0;i<N;i++){
+            pointer = i;     //index of the element it will insert in the correct place
+            temp = A[i];
+        while(temp<A[pointer-1] && pointer>0){
+            A[pointer] = A[pointer-1];
+            pointer--;
+        }
+        A[pointer] = temp;
+    }
+    return 0;
+
+}
+
+int main(){
+
+    /* Taking input */
+    datatype *A,N;
+    //cout<<"Enter the number of elements in the array: ";
+    //cin>>N;
+    N = 200000; //enter the numberOFTestCases here
+    A = (datatype *) malloc(N*sizeof(datatype));  //Can also use calloc()
+    for(int i = 0;i<N;i++) cin>>A[i];
+    /*Bubble Sort  */
+    selection_sort(A,N);
+    for(int i=0;i<N;i++) cout<<"\n"<<A[i]<<" ";
+    float stop_watch = clock();
+    cout<<"\n Execution time :   "<<(stop_watch-start_watch)/CLOCKS_PER_SEC<<" seconds"<<endl;
+    //cin>>N;
+    return 0;
+}
+
+
+
